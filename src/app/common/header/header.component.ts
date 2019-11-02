fb261564ea3a36cecf35661407f30707b9e941cc
Ahmed Khaled <ahmed11khaled93@gmail.com>
2019-11-02 15:52:09 +0200

    first commit

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
